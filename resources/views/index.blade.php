c9189acf739ba065a37af71c337328cd9595a01d
Lorena-Criado <criado.manzaneque@gmail.com>
2021-06-12 13:48:34 +0200

    little update in text component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     
 <div class="text-center">
-    <h1 class="text-3xl text-gray-700 mb-2 uppercase">Últimos cursos</h1>
-    <h2 class="text-xl text-gray-600">Fórmate online como profesional en tecnología</h2>
-    <h3 class="text-lg text-gray-600">70% de los graduados duplican sus ingresos</h3>
+    <h1 class="text-3xl text-gray-700 mb-2 uppercase">Plataforma de cursos</h1>
+    <h2 class="text-xl text-gray-600">Aprendiendo con Livewire y Laravel 8</h2>
+    <h3 class="text-lg text-gray-600">El 70% de los graduados duplican sus ingresos</h3>
 </div>
 
 <livewire:course-list>
