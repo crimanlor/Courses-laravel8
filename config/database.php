8c40e0421f2f5318ee39d51a075f44399b4efe33
Lorena-Criado <criado.manzaneque@gmail.com>
2021-06-12 12:45:41 +0200

    add other modification for solved problems to deploy

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'character_set_server' => 'utf8',
             'prefix' => '',
